b59dc6fb9669ddaeecdb17a49bfcdf8d73606fce
Eugenio Aragón <97991009+euaragon@users.noreply.github.com>
2023-03-20 23:50:50 -0300

    algunas cositas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home/Home.jsx";
+import Register from "./Register/Register";
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Food</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Register/Register.jsx
@@ -0,0 +1,9 @@
+import Zapas from "../images/login-image.jpg"
+
+export default function Register() {
+    return(
+        <div className="register-landing" >
+            <img src={Zapas} alt="" />
+        </div>
+    )
+}

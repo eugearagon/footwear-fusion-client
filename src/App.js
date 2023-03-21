5dfa15a4446642ac68c73a88670f0aeef2ba6ffc
Eugenio Aragón <97991009+euaragon@users.noreply.github.com>
2023-03-21 16:19:44 -0300

    register landing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./Home/Home.jsx";
 import Register from "./Register/Register";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -1,9 +1,21 @@
-import Zapas from "../images/login-image.jpg"
-
+import Zapas from "../images/login-image.jpg";
+import logo from "../images/logo.png";
 export default function Register() {
-    return(
-        <div className="register-landing" >
-            <img src={Zapas} alt="" />
-        </div>
-    )
+  return (
+    <div className="register-landing">
+      <div className="form">
+        <img src={logo} alt="" />
+        <h1>Registrate ¡Tus próximas zapatillas están acá!</h1>
+        <form action="">
+          <label htmlFor="">Email</label><input type="text" />
+          <label htmlFor="">Contraseña</label><input type="text" />
+          <label htmlFor="">Nombre</label><input type="text" />
+          <label htmlFor="">Apellido</label><input type="text" />
+          <label htmlFor="">DNI</label><input type="text" />
+          <button>Enviar</button>
+        </form>
+      </div>
+      <img className="zapas" src={Zapas} alt="" />
+    </div>
+  );
 }

c4d88bfd0e14e34fa5b6525417243f74215c2eac
Eugenio Aragón <97991009+euaragon@users.noreply.github.com>
2023-03-21 18:21:09 -0300

    slider ok le falta responsive

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes, BrowserRouter, useLocation } from "react-router-dom";
 import Home from "./Home/Home.jsx";
 import Register from "./Register/Register";
+// import Navbar from "./NavBar/Navbar"
 
 function App() {
+  // const location = useLocation();
   return (
     <div className="App">
+      {/* {location.pathname !== "/register" && <Navbar/>} */}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />

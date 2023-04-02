c598d3f12f24248f8c81ec8152fb1616d87f2dd5
HugoSchierano <hugoschierano@gmail.com>
2023-04-02 18:17:00 -0300

    reducer

--- src/Redux/Actions/actions.js
+++ src/Redux/Actions/actions.js
@@ -32,3 +32,11 @@ export const DELETE_PRODUCT="DELETE_PRODUCT";
 export const LOG_IN="LOG_IN";
 export const GET_USERS="GET_USERS";
 export const POST_USERS="POST_USERS";
+//acciones del LoginFirebase
+export const POST_INGRESO = "POST_INGRESO";
+export const BORRAR_TOKEN = "BORRAR_TOKEN";
+// export const SET_USUARIO="SET_USUARIO";
+export const POST_REGISTRO = "POST_REGISTRO";
+export const POST_GOOGLE = "POST_GOOGLE";
+// export const GET_USUARIOS = "GET_USUARIOS";
+// export const GET_PRODUCT = "GET_PRODUCT";

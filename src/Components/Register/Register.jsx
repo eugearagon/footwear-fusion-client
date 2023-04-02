00976678f30967f300cc64a209b22f28d2753152
JEBenitez92 <108236690+JEBenitez92@users.noreply.github.com>
2023-04-02 15:30:37 -0300

    varios

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,31 +1,9 @@
 import Zapas from "../images/login-image.jpg";
 import logo from "../images/logo.png";
-import { gapi } from "gapi-script";
-import GoogleLogin from "react-google-login";
-import { useEffect, useState } from "react";
+
 import { NavLink } from "react-router-dom";
 export default function Register() {
-  const clientId =
-    "506527311437-rq6bo8tdhk58pf8nu4f75d315buemvff.apps.googleusercontent.com";
-
-  const [user, setUser] = useState({});
-
-  useEffect(() => {
-    const start = () => {
-      gapi.auth2.init({
-        clientId: clientId,
-      });
-    };
-    gapi.load("client:auth2", start);
-  }, []);
-
-  const onSuccess = (response) => {
-    setUser(response.profileObj);
-  };
-
-  const onFailure = () => {
-    console.log("algo salio mal");
-  };
+  
 
   return (
     <div className="register-landing">
@@ -44,18 +22,8 @@ export default function Register() {
             <button>Enviar</button>
             <button className="favs">Olvidé mi contraseña</button>
           </form>
-          <GoogleLogin
-            className="google-login"
-            clientId={clientId}
-            onSuccess={onSuccess}
-            onFailure={onFailure}
-            cookiePolicy={"single_host_policy"}
-            uxMode={"popup"}
-          />
-          <div className={user ? "profile" : "hidden"}>
-            <img src={user.imageUrl} alt="" />
-            <h3>{user.name}</h3>
-          </div>
+          
+         
         </div>
         <div className="log-admin">
           <h6>Sos Admin? </h6>
@@ -65,6 +33,6 @@ export default function Register() {
         </div>
       </div>
       <img className="zapas" src={Zapas} alt="" />
-    </div>
-  );
+    </div>
+  );
 }

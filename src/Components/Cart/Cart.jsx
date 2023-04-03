6c0766f6441b4b0c031005083054b6e9a89ceb61
JEBenitez92 <108236690+JEBenitez92@users.noreply.github.com>
2023-04-03 16:16:12 -0300

    jodiendo

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,8 +1,44 @@
 import { NavLink } from "react-router-dom";
 import prod from "../images/product.jpg";
 import promos from "../images/promos.jpg";
+import axios from "axios";
+import { useEffect } from "react";
+
 
 export default function Cart() {
+  
+  const data = {
+    item: [
+      {
+        title: 'nike',
+        unit_price: parseInt('500'),
+        quantity: parseInt('2'),
+      },
+      {
+        title: 'toper',
+        unit_price: parseInt('1000'),
+        quantity: parseInt('1'),
+      },
+    ],
+    user: {
+      area_code: "011",
+      number: 11111111,
+      zip_code: "1234",
+      street_name: "piñero",
+      street_number: 1246,
+      email: "jonathan992@hotmail.com",
+      number: "2345895",
+      type: "DNI",
+      name: "Jonathan",
+      surname: "Benitez"
+    }
+  };
+  
+  const mercadoPago = () => {
+    axios.post("http://localhost:3001/mp/create_preference", data)
+      .then((res) => (window.location.href = res.data.global.init_point))
+      .catch((error) => console.log(error))
+  }
   return (
     <div className="cart">
       <div className="cart-header">
@@ -101,7 +137,8 @@ export default function Cart() {
         <div className="ahora-si">
           <span>Subtotal: $6.500</span>
           <h1>Total $6.500</h1>
-          <button>COMPRAR</button>
+          <button onClick={mercadoPago}>COMPRAR</button>
+          
           <NavLink to={"/"}>
             <button className="favs">Continuar comprando...</button>
           </NavLink>

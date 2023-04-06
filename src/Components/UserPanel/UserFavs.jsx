fae513357de8be054dc244241815e6c90a2398b9
Eugenio Aragón <97991009+euaragon@users.noreply.github.com>
2023-04-06 20:30:09 -0300

    cosillas

--- src/Components/UserPanel/UserFavs.jsx
+++ src/Components/UserPanel/UserFavs.jsx
@@ -3,21 +3,19 @@ import fav from "../images/cora.png";
 import { useEffect } from "react";
 import { getFav } from "../../Redux/Actions";
 
-
 export default function UserFavs() {
+  const dispatch = useDispatch();
+  const userId = useSelector((state) => state.loginUser.id);
 
-    // const dispatch = useDispatch()
-    // const userId = useSelector((state) => state.loginUser.id)
+  useEffect(() => {
+    const favoritos = async () => {
+      await dispatch(getFav(userId));
+    };
+    favoritos();
+  }, [userId, dispatch]);
 
-    // useEffect(()=>{
-    //   const favoritos = async () =>{
-    //     await dispatch(getFav(userId))
-    //   }
-    //   favoritos()
-    // },[userId, dispatch])
-
-    const itemFav = useSelector((state) => state.itemFav);
-    console.log("itemFav", itemFav);
+  const itemFav = useSelector((state) => state.itemFav);
+  console.log("itemFav", itemFav);
   return (
     <div className="user-content">
       <div className="user-data">
@@ -25,40 +23,33 @@ export default function UserFavs() {
         <div className="data-list">
           <h6>MIS FAVORITOS</h6>
 
-        
-        {itemFav && itemFav.length > 0 ? (
+          {itemFav && itemFav.length > 0 ? (
             itemFav.map((e) => (
+              <button className="btn-fav">
                 <div className="zapato-fav" key={e.id}>
-                <img src={e.image} alt={e.title} />
-                <div className="zapato-datos-fav">
-                  <p>
-                    <strong>{e.marca}</strong>
-                    <br />
-                    {e.title}
-                  </p>
-                  {/* <small>Código del artículo: {e.code}</small> */}
-                  <p>Precio ${e.price.toLocaleString("de-De")}</p>
-                </div>
-                <div>
-                  <div className="selecciones">
-                   {/* <p>Cantidad: {e.qty}</p>
-                   <p>Talle: {e.qty}</p> */}
-                  </div>
-                  <br />
-                  <div className="botonera">
-                    <button className="comprar">¡Agregar al Carrito!</button>
-                    <button className="favs">Eliminar producto</button>
-                  </div>
-                </div>
-              </div>
-            ))
-        ) : (
-            <div className="zapato-fav">
-          <h1>TODAVIA NO HAY PRODUCTOS</h1>
-        </div>
-        )}
+                  <img src={e.image} alt={e.title} />
+                  <div className="zapato-datos-fav">
+                    <p>
+                      <strong>{e.marca}</strong>
+                      <br />
+                      {e.title}
+                    </p>
 
-         
+                    <p>Precio ${e.price.toLocaleString("de-De")}</p>
+                  </div>
+                  <div>
+                    <div className="botonera">
+                      <button className="favs">Quitar de Favoritos</button>
+                    </div>
+                  </div>
+                </div>
+              </button>
+            ))
+          ) : (
+            <div className="zapato-fav">
+              <h1>TODAVIA NO HAY PRODUCTOS</h1>
+            </div>
+          )}
         </div>
       </div>
     </div>

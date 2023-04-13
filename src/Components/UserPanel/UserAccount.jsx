6f95eca06504b4f1c0c83ee53ab3c686279a28bd
María Emilia Landa <emilia.landa@gmail.com>
2023-04-12 23:35:19 -0300

    edrfweijnfoqwerf

--- src/Components/UserPanel/UserAccount.jsx
+++ src/Components/UserPanel/UserAccount.jsx
@@ -38,6 +38,7 @@ export default function UserAccount() {
   const enviarDatos = async () => {
     try {
       await dispatch(postDataUser(loginUserId, datos))
+      await dispatch(getDatosUser(loginUserId))
     } catch (error) {
       console.log(error.menssage)
     }

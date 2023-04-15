997e6b2474e286020429eefc51d51f99cc82ec95
JEBenitez92 <108236690+JEBenitez92@users.noreply.github.com>
2023-04-15 16:03:55 -0300

    cambio rol en proceso

--- src/Components/admin/Panel/UserManage.jsx
+++ src/Components/admin/Panel/UserManage.jsx
@@ -5,15 +5,35 @@ import userIconAdmin from "../../images/user-icon-admin.png";
 
 import { useDispatch, useSelector } from "react-redux";
 import { getUsers } from "../../../Redux/Actions";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
 
 export default function UserManage() {
   const usuarios = useSelector((state) => state.users);
   const dispatch = useDispatch();
+  const [newState, setNewState] = useState(false)
+  const [newRol, setNewRol] = useState(false)
 
   useEffect(() => {
     dispatch(getUsers());
   }, [dispatch]);
+
+  const cambiarState = () => {
+    setNewState(true);
+  }
+
+  const guardarState = () => {
+    setNewState(false);
+  }
+
+  const cambiarRol = () => {
+    setNewRol(true);
+  }
+
+  const guardarRol = () => {
+    setNewRol(false);
+  }
+
 
   return (
     <div className="admin-content">
@@ -38,6 +58,19 @@ export default function UserManage() {
             ))}</h5>
               <h5>{u.email}</h5>
               <p className={`${u.rol.toLowerCase() === "Blocked" ? "rol-block" : ""}`} >{u.rol}</p>
+              <button onClick={cambiarRol}>Editar Rol</button>
+              {newRol ? <div>
+                <input /> 
+                <button onClick={guardarRol}>guardar rol</button> </div>
+                :null}
+              <p className={`${u.state.toLowerCase() === "Blocked" ? "rol-block" : ""}`}>{u.state}</p>
+              <button onClick={cambiarState}>Editar state</button>
+              {newState ? <div>
+                <input /> 
+                <button onClick={guardarState}>guardar state</button> </div>
+                :null}
+
+             
             </>
             <br /><br />
           </>
